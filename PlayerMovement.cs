39b186794abb002ee91ea020adb89d20a3d5bc87
Higa <codehiga@gmail.com>
2022-08-01 17:10:20 -0300

    initial commit game

---
+++ PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        MyInput();        
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rigidbody.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+}
